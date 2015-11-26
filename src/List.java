c1b028385530c6d1a479fb20991bca9a97910f5b
MarkHu <changdehu@gmail.com>
2015-11-26 19:27:01 +1100

    This just creates the framework of the generic class with the Place holder T been used.

--- src/List.java
+++ src/List.java
@@ -1,5 +1,15 @@
-/**
- * Created by MarkHu on 26/11/2015.
- */
-public class List {
+//have the T there that is a place holder where the data type will be used later on.
+
+public class List<T> {
+    private T[] datastore; //we know its going to be an array but it's not a data type.
+    private int size;
+    private int pos;
+
+    //constructor
+
+    public List(int numElements){
+        size = numElements;
+        pos = 0;
+        datastore = (T[]) new Object[size]; //so we need to create an object array and then type cast it.
+    }
 }
