074bf8c0682853bc77b6c63587ef46e09eaabee0
MarkHu <changdehu@gmail.com>
2015-11-28 16:03:06 +1100

    example of how to make generic node classes

---
+++ src/Exercise/Node.java
@@ -0,0 +1,15 @@
+package Exercise;
+
+public class Node<T> {
+    private T data;
+    public Node next;
+
+    public Node(T data){
+        this.data = data;
+        next = null;
+    }
+
+    T getData(){
+        return data;
+    }
+}
